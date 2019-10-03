83eab2c4c684c7039858131a1b2bab029760810a
Diego Barrio H <diegobarrioh@gmail.com>
2019-10-03 21:31:10 +0200

    añadido campo esCorrecta en la respuesta

--- src/main/java/com/dbhacademy/firefight/model/entity/Respuesta.java
+++ src/main/java/com/dbhacademy/firefight/model/entity/Respuesta.java
@@ -1,54 +1,58 @@
 package com.dbhacademy.firefight.model.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-import javax.validation.constraints.Size;
-
 import com.dbhacademy.firefight.model.BaseEntity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "TB04_RESPUESTA", schema = "BDD_FIREFIGHT")
 public class Respuesta extends BaseEntity {
 
-	private static final long serialVersionUID = 4L;
+    private static final long serialVersionUID = 4L;
 
-	@ManyToOne
-	@JoinColumn(name = "PREGUNTA_ID", referencedColumnName = "ID")
-	@JsonIgnore
-	private Pregunta pregunta;
+    @ManyToOne
+    @JoinColumn(name = "PREGUNTA_ID", referencedColumnName = "ID")
+    @JsonIgnore
+    private Pregunta pregunta;
 
-	@Size(max = 50)
-	@Column(unique = true)
-	private String codigo;
-	@Size(max = 255)
-	private String texto;
+    @Size(max = 50)
+    @Column(unique = true)
+    private String codigo;
+    @Size(max = 255)
+    private String texto;
+    private boolean esCorrecta;
 
-	public Pregunta getPregunta() {
-		return pregunta;
-	}
+    public Pregunta getPregunta() {
+        return pregunta;
+    }
 
-	public void setPregunta(Pregunta pregunta) {
-		this.pregunta = pregunta;
-	}
+    public void setPregunta(Pregunta pregunta) {
+        this.pregunta = pregunta;
+    }
 
-	public String getCodigo() {
-		return codigo;
-	}
+    public String getCodigo() {
+        return codigo;
+    }
 
-	public void setCodigo(String codigo) {
-		this.codigo = codigo;
-	}
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
 
-	public String getTexto() {
-		return texto;
-	}
+    public String getTexto() {
+        return texto;
+    }
 
-	public void setTexto(String texto) {
-		this.texto = texto;
-	}
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
 
+    public boolean esCorrecta() {
+        return esCorrecta;
+    }
+
+    public void setCorrecta(boolean correcta) {
+        esCorrecta = correcta;
+    }
 }
--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -5,7 +5,7 @@ Insert into BDD_FIREFIGHT.TB02_TEMA(ID,CODIGO,TEXTO,AGRUPACION_ID) values (1,'TM
 
 Insert into BDD_FIREFIGHT.TB03_PREGUNTA(ID,CODIGO,TEXTO,TEMA_ID) values (1,'PR0001','Sólo uno de los siguientes principios está garantizado constitucionalmente:',1);
 
-Insert into BDD_FIREFIGHT.TB04_RESPUESTA(ID,CODIGO,TEXTO,PREGUNTA_ID) values (1,'RE0001','La interdicción de la arbitrariedad del poder judicial.',1);
-Insert into BDD_FIREFIGHT.TB04_RESPUESTA(ID,CODIGO,TEXTO,PREGUNTA_ID) values (2,'RE0002','La responsabilidad e interdicción de la arbitrariedad de la Administración Pública, tanto nacional, como autonómica o local',1);
-Insert into BDD_FIREFIGHT.TB04_RESPUESTA(ID,CODIGO,TEXTO,PREGUNTA_ID) values (3,'RE0003','La responsabilidad de la Administración Pública.',1);
-Insert into BDD_FIREFIGHT.TB04_RESPUESTA(ID,CODIGO,TEXTO,PREGUNTA_ID) values (4,'RE0004','La responsabilidad e interdicción de la arbitrariedad de los poderes públicos.',1);
+Insert into BDD_FIREFIGHT.TB04_RESPUESTA(ID,CODIGO,TEXTO,ES_CORRECTA,PREGUNTA_ID) values (1,'RE0001','La interdicción de la arbitrariedad del poder judicial.',false,1);
+Insert into BDD_FIREFIGHT.TB04_RESPUESTA(ID,CODIGO,TEXTO,ES_CORRECTA,PREGUNTA_ID) values (2,'RE0002','La responsabilidad e interdicción de la arbitrariedad de la Administración Pública, tanto nacional, como autonómica o local',false,1);
+Insert into BDD_FIREFIGHT.TB04_RESPUESTA(ID,CODIGO,TEXTO,ES_CORRECTA,PREGUNTA_ID) values (3,'RE0003','La responsabilidad de la Administración Pública.',false,1);
+Insert into BDD_FIREFIGHT.TB04_RESPUESTA(ID,CODIGO,TEXTO,ES_CORRECTA,PREGUNTA_ID) values (4,'RE0004','La responsabilidad e interdicción de la arbitrariedad de los poderes públicos.',true,1);
