00452437a375eb17d59a925d8288f4da05bd5ef4
Diego Barrio H <diegobarrioh@gmail.com>
2019-10-05 18:23:24 +0200

    Rellenados más registros de la bbdd

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,7 +1,45 @@
-Insert into BDD_FIREFIGHT.TB01_AGRUPACION(ID,CODIGO,TEXTO) values (1,'AG001','LEGISLACIÓN');
+Insert into BDD_FIREFIGHT.TB01_AGRUPACION(ID,CODIGO,TEXTO) values (1,'AG001','PARTE GENERAL');
+Insert into BDD_FIREFIGHT.TB01_AGRUPACION(ID,CODIGO,TEXTO) values (2,'AG002','BLOQUE I. TEMAS GENERALES');
+Insert into BDD_FIREFIGHT.TB01_AGRUPACION(ID,CODIGO,TEXTO) values (3,'AG003','BLOQUE II. INCENDIOS');
+Insert into BDD_FIREFIGHT.TB01_AGRUPACION(ID,CODIGO,TEXTO) values (4,'AG004','BLOQUE III. SALVAMETOS');
+Insert into BDD_FIREFIGHT.TB01_AGRUPACION(ID,CODIGO,TEXTO) values (5,'AG005','BLOQUE IV. RIESGOS TECNOLÓGICOS');
+Insert into BDD_FIREFIGHT.TB01_AGRUPACION(ID,CODIGO,TEXTO) values (6,'AG006','BLOQUE V. MATERIALES Y EQUIPOS');
 
 
-Insert into BDD_FIREFIGHT.TB02_TEMA(ID,CODIGO,TEXTO,AGRUPACION_ID) values (1,'TM0001','Estatuto de Autonomía de la CAM',1);
+Insert into BDD_FIREFIGHT.TB02_TEMA(ID,CODIGO,TEXTO,AGRUPACION_ID) values (1,'TM0001','Constitución Española',1);
+Insert into BDD_FIREFIGHT.TB02_TEMA(ID,CODIGO,TEXTO,AGRUPACION_ID) values (2,'TM0002','Ley de Bases de Régimen Local',1);
+Insert into BDD_FIREFIGHT.TB02_TEMA(ID,CODIGO,TEXTO,AGRUPACION_ID) values (3,'TM0003','Estatuto de Autonomía de la CV',1);
+Insert into BDD_FIREFIGHT.TB02_TEMA(ID,CODIGO,TEXTO,AGRUPACION_ID) values (4,'TM0004','Estatuto Básico del Empleado Público',1);
+Insert into BDD_FIREFIGHT.TB02_TEMA(ID,CODIGO,TEXTO,AGRUPACION_ID) values (5,'TM0005','Ley de Incompatibilidades',1);
+Insert into BDD_FIREFIGHT.TB02_TEMA(ID,CODIGO,TEXTO,AGRUPACION_ID) values (6,'TM0006','Ley de Prevención de Riesgos Laborales',1);
+Insert into BDD_FIREFIGHT.TB02_TEMA(ID,CODIGO,TEXTO,AGRUPACION_ID) values (7,'TM0007','Ley de Procedimiento Administrativo',1);
+Insert into BDD_FIREFIGHT.TB02_TEMA(ID,CODIGO,TEXTO,AGRUPACION_ID) values (8,'TM0008','Ley Reguladora de las Haciendas Locales',1);
+
+
+Insert into BDD_FIREFIGHT.TB02_TEMA(ID,CODIGO,TEXTO,AGRUPACION_ID) values (9,'TM0009','El servicio de extinción de incendios',2);
+Insert into BDD_FIREFIGHT.TB02_TEMA(ID,CODIGO,TEXTO,AGRUPACION_ID) values (10,'TM0010','Cellejero',2);
+Insert into BDD_FIREFIGHT.TB02_TEMA(ID,CODIGO,TEXTO,AGRUPACION_ID) values (11,'TM0011','Ordenanza fiscal de las Tasas',2);
+Insert into BDD_FIREFIGHT.TB02_TEMA(ID,CODIGO,TEXTO,AGRUPACION_ID) values (12,'TM0012','SPEIS Comunitat Valenciana',2);
+Insert into BDD_FIREFIGHT.TB02_TEMA(ID,CODIGO,TEXTO,AGRUPACION_ID) values (13,'TM0013','Plan territorial de Emergencias',2);
+
+Insert into BDD_FIREFIGHT.TB02_TEMA(ID,CODIGO,TEXTO,AGRUPACION_ID) values (14,'TM0014','Medios de extinción',3);
+Insert into BDD_FIREFIGHT.TB02_TEMA(ID,CODIGO,TEXTO,AGRUPACION_ID) values (15,'TM0015','RIPCI',3);
+Insert into BDD_FIREFIGHT.TB02_TEMA(ID,CODIGO,TEXTO,AGRUPACION_ID) values (16,'TM0016','Incendios en interiores',3);
+Insert into BDD_FIREFIGHT.TB02_TEMA(ID,CODIGO,TEXTO,AGRUPACION_ID) values (17,'TM0017','Equipos de salvamento',3);
+Insert into BDD_FIREFIGHT.TB02_TEMA(ID,CODIGO,TEXTO,AGRUPACION_ID) values (18,'TM0018','Ventilación',3);
+Insert into BDD_FIREFIGHT.TB02_TEMA(ID,CODIGO,TEXTO,AGRUPACION_ID) values (19,'TM0019','Incendios Forestales',3);
+
+
+Insert into BDD_FIREFIGHT.TB02_TEMA(ID,CODIGO,TEXTO,AGRUPACION_ID) values (20,'TM0020','Rescate en accidentes de tráfico',4);
+Insert into BDD_FIREFIGHT.TB02_TEMA(ID,CODIGO,TEXTO,AGRUPACION_ID) values (21,'TM0021','Rescate acuático',4);
+Insert into BDD_FIREFIGHT.TB02_TEMA(ID,CODIGO,TEXTO,AGRUPACION_ID) values (22,'TM0022','El riesgo de inundaciones',4);
+Insert into BDD_FIREFIGHT.TB02_TEMA(ID,CODIGO,TEXTO,AGRUPACION_ID) values (23,'TM0023','Elevadores (Ascensores)',4);
+Insert into BDD_FIREFIGHT.TB02_TEMA(ID,CODIGO,TEXTO,AGRUPACION_ID) values (24,'TM0024','Principios de la construcción',4);
+Insert into BDD_FIREFIGHT.TB02_TEMA(ID,CODIGO,TEXTO,AGRUPACION_ID) values (25,'TM0025','CTE DB-SI',4);
+Insert into BDD_FIREFIGHT.TB02_TEMA(ID,CODIGO,TEXTO,AGRUPACION_ID) values (26,'TM0026','Patología de la edificación',4);
+Insert into BDD_FIREFIGHT.TB02_TEMA(ID,CODIGO,TEXTO,AGRUPACION_ID) values (27,'TM0027','Urgencias sanitarias',4);
+
+
 
 Insert into BDD_FIREFIGHT.TB03_PREGUNTA(ID,CODIGO,TEXTO,TEMA_ID) values (1,'PR0001','Sólo uno de los siguientes principios está garantizado constitucionalmente:',1);
 
