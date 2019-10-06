ca61dfb095dc7c66efb5b5b7fa5705738facb85b
Diego Barrio H <diegobarrioh@gmail.com>
2019-10-06 12:56:46 +0200

    Añadido soporte para un Objeto Test en front y back.

--- src/main/java/com/dbhacademy/firefight/controller/FrontEndController.java
+++ src/main/java/com/dbhacademy/firefight/controller/FrontEndController.java
@@ -1,15 +1,22 @@
 package com.dbhacademy.firefight.controller;
 
+import com.dbhacademy.firefight.model.Test;
 import com.dbhacademy.firefight.model.entity.Agrupacion;
 import com.dbhacademy.firefight.service.AgrupacionService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class FrontEndController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(FrontEndController.class);
 
     private AgrupacionService agrupacionService;
 
@@ -21,6 +28,20 @@ public class FrontEndController {
     @RequestMapping("/")
     public String index(Model model){
         model.addAttribute("agrupaciones", this.agrupacionService.getAgrupaciones());
+        model.addAttribute("test", new Test());
         return "index";
+    }
+
+    @GetMapping("/test")
+    public String pregunta(Model model){
+        LOG.info("vamos por get al test");
+        model.addAttribute("test", new Test());
+        return "test";
+    }
+
+    @PostMapping("/test")
+    public String pregunta(@ModelAttribute Test test){
+        LOG.info("recibo el form test con {} preguntas por tema", test.getNumPreguntasPorTema());
+        return "test";
     }
 }
---
+++ src/main/resources/static/js/test.js
@@ -0,0 +1,18 @@
+var Test = (function () {
+
+    //modo estricto
+    "use strict";
+    //elementos del Test
+
+    var tiempoTotalResolucion,tiempoResolucionPregunta;
+
+    return {
+
+        init: function (temas) {
+
+            alert("pues eso que empezamos.... me han llegado " + temas + " preguntas por tema");
+
+        }
+    }
+
+}());
